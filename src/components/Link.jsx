2585076c1ab0e9c3450e3bcae9e220d195ac46a0
elena_kau <lelickau@yandex.ru>
2022-07-03 17:04:45 +0200

    Added tests Jest and react-testing-library

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import {useState} from 'react';
 
-export default function Link(props) {
-  if (props.name) {
-    return <h1>Hello, {props.name}!</h1>;
-  } else {
-    return <span>Hey, stranger</span>;
-  }
+const STATUS = {
+  HOVERED: 'hovered',
+  NORMAL: 'normal',
+};
+
+export default function Link({page, children}) {
+  const [status, setStatus] = useState(STATUS.NORMAL);
+
+  const onMouseEnter = () => {
+    setStatus(STATUS.HOVERED);
+  };
+
+  const onMouseLeave = () => {
+    setStatus(STATUS.NORMAL);
+  };
+
+  return (
+    <a
+      className={status}
+      href={page || '#'}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      {children}
+    </a>
+  );
 }
